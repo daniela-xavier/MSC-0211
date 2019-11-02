96ee900ac0917fe4495d8b504eff60c87b57bed5
daniela-xavier <daniela.xavier.con@outlook.com.br>
2019-11-02 16:38:51 -0300

    aula

---
+++ eats-pagamento-service/src/main/java/eatspagamentoservice/PagamentoRepository.java
@@ -0,0 +1,7 @@
+package eatspagamentoservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+}
---
+++ eats-pagamento-service/src/test/java/eatspagamentoservice/EatsPagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eatspagamentoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsPagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
